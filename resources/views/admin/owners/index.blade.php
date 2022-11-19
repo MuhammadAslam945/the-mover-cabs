3b6667978655655776ed30a9e0bc73a7e9564deb
Muhammad Aslam <81037354+MuhammadAslam945@users.noreply.github.com>
2022-11-20 04:32:37 +0500

    Update index.blade.php
    index has been updated

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -13,14 +13,13 @@
                     <div class="page-title">Dashboard</div>
                 </div>
                 <ol class="breadcrumb page-breadcrumb pull-right">
-                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="index.html">Home</a>&nbsp;<i
-                            class="fa fa-angle-right"></i>
+                    <li><i class="fa fa-home"></i>&nbsp;<a class="parent-item" href="index.html">Home</a>&nbsp;<i class="fa fa-angle-right"></i>
                     </li>
                     <li class="active">Dashboard</li>
                 </ol>
             </div>
         </div>
-        {{-- <div class="container-fluid"> --}}
+        <div class="container-fluid">
 
             <div class="row">
                 <div class="col-12">
@@ -28,38 +27,33 @@
 
                         <div class="box-header with-border">
 
-                            <div>
-                                <strong class="text-danger" style="font-size: 18px;">Note: We are currently working on
-                                    this
-                                    update, you may get this update soon</strong>
-                            </div><br>
+
                             <div class="row text-right">
 
 
                                 <div class="col-2">
                                     <div class="form-group">
-                                        <input type="text" id="search_keyword" name="search" class="form-control"
-                                            placeholder="@lang('view_pages.enter_keyword')">
+                                        <input type="text" id="search_keyword" name="search" class="form-control" placeholder="@lang('view_pages.enter_keyword')">
                                     </div>
                                 </div>
 
-                                <div class="col-1">
-                                    <button class="btn btn-success btn-outline btn-sm mt-5" type="submit">
+                                <div class="col-2">
+                                    <button class="btn btn-success btn-outline btn-sm mr-2" type="submit">
                                         @lang('view_pages.search')
                                     </button>
                                 </div>
 
-                                <div class="col-9 text-right">
+                                <div class="col-8 text-right">
                                     <a href="{{ url('owners/create',$area->id)}}" class="btn btn-primary btn-sm">
-                                        <i class="mdi mdi-plus-circle mr-2"></i>@lang('view_pages.add')</a>
-                                    <!--  <a class="btn btn-danger">
-                                    Export</a> -->
+                                        <i class="mdi mdi-plus-circle ml-4 mr-2"></i>@lang('view_pages.add')</a>
+                                    <a class="btn btn-danger btn-sm">
+                                        Export</a>
                                 </div>
-                                <!-- <div class="box-controls pull-right">
-                <div class="lookup lookup-circle lookup-right">
-                  <input type="text" name="s">
-                </div>
-              </div> -->
+                                <div class="box-controls pull-right">
+                                    <div class="lookup lookup-circle lookup-right">
+                                        <input type="text" name="s">
+                                    </div>
+                                </div>
                             </div>
 
 
@@ -79,8 +73,8 @@
             </div>
         </div>
     </div>
-    {{--
-</div>--}}
+
+</div>
 <!-- container -->
 
 
@@ -89,26 +83,26 @@
 <script src="{{asset('assets/js/fetchdata.min.js')}}"></script>
 <script>
     $(function() {
-    $('body').on('click', '.pagination a', function(e) {
-    e.preventDefault();
-    var url = $(this).attr('href');
-    $.get(url, $('#search').serialize(), function(data){
-        $('#js-project-partial-target').html(data);
-    });
-});
-
-$('#search').on('click', function(e){
-    e.preventDefault();
-        var search_keyword = $('#search_keyword').val();
-        console.log(search_keyword);
-        fetch('project/fetch?search='+search_keyword)
-        .then(response => response.text())
-        .then(html=>{
-            document.querySelector('#js-project-partial-target').innerHTML = html
+        $('body').on('click', '.pagination a', function(e) {
+            e.preventDefault();
+            var url = $(this).attr('href');
+            $.get(url, $('#search').serialize(), function(data) {
+                $('#js-project-partial-target').html(data);
+            });
         });
-});
 
-});
+        $('#search').on('click', function(e) {
+            e.preventDefault();
+            var search_keyword = $('#search_keyword').val();
+            console.log(search_keyword);
+            fetch('project/fetch?search=' + search_keyword)
+                .then(response => response.text())
+                .then(html => {
+                    document.querySelector('#js-project-partial-target').innerHTML = html
+                });
+        });
+
+    });
 </script>
 
 @endsection
